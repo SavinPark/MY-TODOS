08ca853e67aa662424aa308ec08333192dd60c3a
Savin <jinygreen@naver.com>
2021-12-30 21:55:51 +0900

    초안 완성

--- README.md
+++ README.md
@@ -1,9 +1,34 @@
 # MY-TODOS
 HTML, CSS 그리고 Javascript로 만든 투두리스트입니다.
 
-# 기능
+## 기능
 - 날짜 및 시간 표현
 - 등록
 - 완료/미완료 상태 표시
 - 수정
 - 삭제
+
+## 구조
+### Header
+- year, month, date
+- day, time(hour:minute)
+### Main -- todos
+- 할 일
+- 완료/미완료 아이콘
+- 수정 아이콘
+- 삭제 아이콘
+### Main -- form
+- 제목
+- 내용
+### Footer -- todos
+- 추가 버튼 ( + )
+### Footer -- form
+- 저장 버튼 ( save )
+- 취소 버튼 ( x ) 
+
+## todolist.js
+(자바스크립트 부가설명 추가 예정)
+
+## 앞으로 추가할 기능
+달력 표시 기능과 특정 날짜를 선택하면 그날의 todos를 나타내는 기능
+
--- todolist.js
+++ todolist.js
@@ -1,4 +1,10 @@
 // DOM
+const year = document.getElementById('year');
+const month = document.getElementById('month');
+const date = document.getElementById('date');
+const day = document.getElementById('day');
+const time = document.getElementById('time');
+
 const saveBtn = document.querySelector('.save-btn'); 
 const addBtn = document.querySelector('.plus-circle'); 
 
@@ -8,6 +14,7 @@ const todosList = document.querySelector('.todos > ul');
 const form = document.querySelector('.form');
 const input = document.querySelector('input');
 const textarea = document.querySelector('textarea');
+
 
 // TodosArray
 const Todos = [];
@@ -20,9 +27,9 @@ function showTodos() {
   li.innerHTML = `
     <li class="todo">
       <div class="todo--left">
+        <i class="far fa-circle circle-btn on"></i>
         <i class="far fa-check-circle checkcircle-btn"></i>
-        <i class="far fa-circle checkcircle-btn"></i>
-        ${Todos[n-1].title}
+        <span class="work">${Todos[n-1].title}</span>
       </div>
       <div class="todo--right">
         <i class="fas fa-pen edit-btn"></i>
@@ -33,8 +40,59 @@ function showTodos() {
   todosList.prepend(li);
 }
 
+// Today
+Today();
+function Today() {
+  const today = new Date();
+  year.innerHTML = `${today.getFullYear()}`;
+  switch(today.getMonth()) {
+    case 1:
+      month.innerHTML = 'January';
+    case 2:
+      month.innerHTML = 'February';
+    case 3:
+      month.innerHTML = 'March';
+    case 4:
+      month.innerHTML = 'April';
+    case 5:
+      month.innerHTML = 'May';
+    case 6:
+      month.innerHTML = 'June';
+    case 7:
+      month.innerHTML = 'July';
+    case 8:
+      month.innerHTML = 'August';
+    case 9:
+      month.innerHTML = 'September';
+    case 10:
+      month.innerHTML = 'October';
+    case 11:
+      month.innerHTML = 'November';
+    case 12:
+      month.innerHTML = 'December';
+  };
+  date.innerHTML = `${today.getDate()}`;
+  switch(today.getDay()) {
+    case 0:
+      day.innerHTML = 'Sunday';
+    case 1:
+      day.innerHTML = 'Monday';
+    case 2:
+      day.innerHTML = 'Tuesday';
+    case 3:
+      day.innerHTML = 'Wednesday';
+    case 4:
+      day.innerHTML = 'Thursday';
+    case 5:
+      day.innerHTML = 'Friday';
+    case 6:
+      day.innerHTML = 'Saturday';
+  };
+  time.innerHTML = `${today.getHours()}:${today.getMinutes()}`;
+}
 
 // Event Handling
+
 saveBtn.addEventListener('click', () => {
   const title = input.value;
   const contents = textarea.value;
@@ -52,16 +110,57 @@ saveBtn.addEventListener('click', () => {
       title: title,
       contents: contents,
       done: false,
+      edit: false,
+      remove: false,
     });
   }
   // reset input & textarea
   input.value = null;
   textarea.value = null;
   
-  console.log(Todos); // 확인!!!!!!!!!
+  // console.log(Todos); // 확인!!!!!!!!!
 
   showTodos();
+
+  // 생성된 후 선언 가능!
+  const work = document.querySelector('.work');
+  const circleBtn = document.querySelector('.circle-btn');
+  const checkcircleBtn = document.querySelector('.checkcircle-btn');
+  const editBtn = document.querySelector('.edit-btn');
+  const removeBtn = document.querySelector('.remove-btn');
+
+  // 완료/미완료
+  circleBtn.addEventListener('click', (e) => {
+    circleBtn.classList.toggle('on');
+    checkcircleBtn.classList.toggle('on');
+    work.classList.toggle('done');
+  });
+  checkcircleBtn.addEventListener('click', () => {
+    circleBtn.classList.toggle('on');
+    checkcircleBtn.classList.toggle('on');
+    work.classList.toggle('done');
+  });
+
+  // 삭제
+  removeBtn.addEventListener('click', (e) => {
+    e.target.parentElement.parentElement.remove();
+  });
+
+  // 수정
+  editBtn.addEventListener('click', (e) => {
+    e.target.parentElement.parentElement.remove();
+
+    form.classList.toggle('on');
+    todos.classList.toggle('on');
+    addBtn.classList.toggle('on');
+    saveBtn.classList.toggle('on');
+    
+    console.log(e.target.parentElement.parentElement);
+    input.value = title;
+    textarea.value = contents;
+  });
 });
+
 
 addBtn.addEventListener('click',() => {
   addBtn.classList.toggle('on');
